c636bcda5dfe7c2077d94b6cbce03534dd9a1810
krasnof.kobi@gmail.com <krasnoff.kobi@gmail.com>
2017-03-08 19:25:22 +0200

    bug fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import 'hammerjs';
-import { ReactiveFormsModule } from '@angular/forms'
+
 import {FlexLayoutModule} from "@angular/flex-layout";
 
 import { AppComponent } from './app.component';
