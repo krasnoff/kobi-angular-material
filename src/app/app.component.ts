0acdf372bfd6c7bc2eae051bc5df5a6c157aeaf0
krasnof.kobi@gmail.com <krasnoff.kobi@gmail.com>
2017-03-12 15:35:18 +0200

    finish auto complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,11 @@ import {FormControl} from '@angular/forms';
 import {Observable} from 'rxjs/Observable';
 import {HttpService} from './app.component.service';
 
+// Observable operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,9 +16,9 @@ import {HttpService} from './app.component.service';
       'openClose',
       [
         state('collapsed, void', style({width: '0px'})),
-        state('expanded', style({width: '170px'})),
+        state('expanded', style({width: '300px'})),
         transition(
-            'collapsed <=> expanded', [animate(300, style({width: '170px'})), animate(300)])
+            'collapsed <=> expanded', [animate(300, style({width: '300px'})), animate(300)])
       ])],
   providers: [HttpService]
 })
@@ -26,84 +31,48 @@ export class AppComponent {
   }
 
   txtSearchCity = new FormControl();
-  options = [
-    'Alabama',
-    'Alaska',
-    'Arizona',
-    'Arkansas',
-    'California',
-    'Colorado',
-    'Connecticut',
-    'Delaware',
-    'Florida',
-    'Georgia',
-    'Hawaii',
-    'Idaho',
-    'Illinois',
-    'Indiana',
-    'Iowa',
-    'Kansas',
-    'Kentucky',
-    'Louisiana',
-    'Maine',
-    'Maryland',
-    'Massachusetts',
-    'Michigan',
-    'Minnesota',
-    'Mississippi',
-    'Missouri',
-    'Montana',
-    'Nebraska',
-    'Nevada',
-    'New Hampshire',
-    'New Jersey',
-    'New Mexico',
-    'New York',
-    'North Carolina',
-    'North Dakota',
-    'Ohio',
-    'Oklahoma',
-    'Oregon',
-    'Pennsylvania',
-    'Rhode Island',
-    'South Carolina',
-    'South Dakota',
-    'Tennessee',
-    'Texas',
-    'Utah',
-    'Vermont',
-    'Virginia',
-    'Washington',
-    'West Virginia',
-    'Wisconsin',
-    'Wyoming'
-   ];
-   filteredOptions: any;
-   getData: string[];
+  options = [];
+  filteredOptions: any;
+  getData: string;
 
-   ngOnInit() {
-      this.filteredOptions = this.txtSearchCity.valueChanges 
-         .startWith(null)
-         .map(name => this.filter(name));
+  ngOnInit() {
+      this.filteredOptions = this.txtSearchCity.valueChanges
+        .debounceTime(200) // wait 200ms after each keystroke before considering the term
+        .distinctUntilChanged() // ignore if next search term is same as previous
+        .switchMap(name => { 
+          if (name.length > 2) 
+            return this._httpService.getMethod('http://gd.geobytes.com/AutoCompleteCity?callback=JSONP_CALLBACK&sort=size&q=' + encodeURIComponent(name) + '&_=1489163270176') // return the http search observable
+          else
+            return Observable.of<string[]>([]) // or the observable of empty heroes if there was no search term
+        })
+        .catch(error => {
+          // TODO: add real error handling
+          console.log(error);
+          return Observable.of<string[]>([]);
+        })
+  }
 
+  /*filter(val: string): string[] {
       // test my new service
-      this._httpService.getMethod('http://gd.geobytes.com/AutoCompleteCity?callback=JSONP_CALLBACK&sort=size&q=new&_=1489163270176')
+      this._httpService.getMethod('http://gd.geobytes.com/AutoCompleteCity?callback=JSONP_CALLBACK&sort=size&q=' + encodeURIComponent(val) + '&_=1489163270176')
         .subscribe(
-          data => this.getData = data,
+          data => this.getData = JSON.stringify(data),
           error => alert("Error: " + error._body),
-          () => console.log('finished') 
+          () => {
+            console.log('finished') 
+          }
         );
-   }
+      
+      //this.options = this.getData ? JSON.parse(this.getData) : [];
+      return this.getData ? JSON.parse(this.getData) : [];
+  }*/
 
-   filter(val: string): string[] {
-      return this.options.filter(option => new RegExp(val, 'gi').test(option)); 
-   }
+  selected(val: string): void {
+      // TODO continue from here
+      debugger;
+  }
 
-   selected(val: string): void {
-      //debugger;
-   }
-
-   showSearchTextBox(val: string): void {
+  showSearchTextBox(val: string): void {
       this.stateExpression = (this.stateExpression != 'expanded') ? 'expanded' : 'collapsed';
-   }
+  }
 }
