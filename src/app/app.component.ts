21c236a7838d9af781475c3766212170602626ae
krasnof.kobi@gmail.com <krasnoff.kobi@gmail.com>
2017-03-24 23:27:11 +0300

    last changes and first deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -107,13 +107,3 @@ export class AppComponent {
   }
 }
 
-@Pipe({name: 'keys'})
-export class KeysPipe implements PipeTransform {
-  transform(value, args:string[]) : any {
-    let keys = [];
-    for (let key in value) {
-      keys.push({key: key, value: value[key]});
-    }
-    return keys;
-  }
-}
